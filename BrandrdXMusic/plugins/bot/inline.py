ef05e8c914669e338ec57d4b8825d34c00792c8d
Eren Yeager <erenbruhhh@gmail.com>
2025-01-21 10:48:24 +0530

    Update inline.py

--- BrandrdXMusic/plugins/bot/inline.py
+++ BrandrdXMusic/plugins/bot/inline.py
@@ -36,22 +36,21 @@ async def inline_query_handler(client, query):
                 [
                     [
                         InlineKeyboardButton(
-                            text="ʏᴏᴜᴛᴜʙᴇ 🎄",
+                            text="YouTube",
                             url=link,
                         )
                     ],
                 ]
             )
-            searched_text = f"""
-❄ <b>ᴛɪᴛʟᴇ :</b> <a href={link}>{title}</a>
+            searched_text = f"""<b>Title :</b> <a href={link}>{title}</a>
 
-⏳ <b>ᴅᴜʀᴀᴛɪᴏɴ :</b> {duration} ᴍɪɴᴜᴛᴇs
-👀 <b>ᴠɪᴇᴡs :</b> <code>{views}</code>
-🎥 <b>ᴄʜᴀɴɴᴇʟ :</b> <a href={channellink}>{channel}</a>
-⏰ <b>ᴘᴜʙʟɪsʜᴇᴅ ᴏɴ :</b> {published}
+<b>Duration :</b> {duration} Minutes 
+<b>Views :</b> <code>{views}</code>
+<b>Channel :</b> <a href={channellink}>{channel}</a>
+<b>Published On :</b> {published}
 
 
-<u><b>➻ ɪɴʟɪɴᴇ sᴇᴀʀᴄʜ ᴍᴏᴅᴇ ʙʏ {app.name}</b></u>"""
+<u><b>Inline Search Mode By {app.name}</b></u>"""
             answers.append(
                 InlineQueryResultPhoto(
                     photo_url=thumbnail,
